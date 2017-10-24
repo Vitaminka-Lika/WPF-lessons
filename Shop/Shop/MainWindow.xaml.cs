2949eaa1423f3b4e0476368d7f537ac91c7c5b1c
Vitaminka-Lika <sarbassova_alika2001@mail.ru>
2017-10-24 21:41:06 +0600

    Binding

---
+++ Shop/Shop/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Shop
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<Product> products = new ObservableCollection<Product>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            productsGrid.ItemsSource = products;
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            double price;
+            bool isParsed = Double.TryParse(priceProductTextBox.Text,out price);
+            if (isParsed)
+            {
+                Product product = new Product()
+                {
+                    Id = products.LongCount(),
+                    Name = nameProductTextBox.Text,
+                    Price = price,
+                    ShelfLife = shelfLifedatePicker.DisplayDate.ToShortDateString()
+                };
+
+                products.Add(product);
+            }
+        }
+    }
+}
---
+++ Shop/Shop/Product.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop
+{
+    public class Product : INotifyPropertyChanged
+    {
+        private long id;
+        private string name;
+        private double price;
+        private string shelfLife;
+        public long Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+        public double Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                price = value;
+                OnPropertyChanged("Price");
+            }
+        }
+        public string ShelfLife
+        {
+            get
+            {
+                return shelfLife;
+            }
+            set
+            {
+                shelfLife = value;
+                OnPropertyChanged("ShelfLife");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string property = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
